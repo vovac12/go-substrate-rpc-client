2ddc8de0e42a54d4d286bbeb304128cbbc64fea0
Vladimir Stepanenko <vovac12@gmail.com>
2021-10-18 12:34:42 +0300

    Add digest hash to leaf

--- types/mmr.go
+++ types/mmr.go
@@ -75,6 +75,7 @@ type MMRLeaf struct {
 	ParentNumberAndHash   ParentNumberAndHash
 	BeefyNextAuthoritySet BeefyNextAuthoritySet
 	ParachainHeads        H256
+	DigestHash            H256
 }
 
 type MMRLeafVersion U8
